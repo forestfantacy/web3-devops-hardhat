a13a81086d19e41cfd87d4e3e7dc61eee2856195
forestfantacy <13717507049@163.com>
2023-02-04 21:35:14 +0800

    init

---
+++ test/uniswap_test.js
@@ -0,0 +1,97 @@
+const {time,loadFixture,} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+const { ethers,network } = require("hardhat");
+const { ChainId, Fetcher, WETH, Route, Trade, TokenAmount, TradeType, Percent } = require('@uniswap/sdk');
+const { ethers } = require("ethers");
+
+describe("", function () {
+
+  const mainnet_UniswapV2Router02_Address = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+  const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+  const DAI_WHALE = '0xF977814e90dA44bFA03b6295A0616a897441aceC';
+  const to = "0xa894026777645913E829fB2AE3dCd6252ee13e01";
+
+  describe("testxxx", function () {
+
+    it("testcase: DAI_WHALE send DAI to RECEIVE", async function () {
+
+        //冒充DAI
+        await network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [DAI]
+        })
+    
+        //冒充DAI_WHALE
+        await network.provider.request({
+          method: "hardhat_impersonateAccount",
+          params: [DAI_WHALE]
+          })
+        const daiWhileSigner= await ethers.getSigner(DAI_WHALE);
+
+        await network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [mainnet_UniswapV2Router02_Address]
+            })
+
+        //根据网络和地址获取DAI
+        const dai = await Fetcher.fetchTokenData(chainId, mainnet_dai_Address, provider);
+        //从sdk 直接取出主网的weth信息
+        const weth = WETH[chainId];
+        console.log("weth name:",weth.name,'addr:',weth.address);
+       
+        //定义交易对 用dai 换 weth
+        const pair = await Fetcher.fetchPairData(dai, weth);
+        console.log("pair liquidityToken:",pair.liquidityToken);
+
+        //定义交易路径，这里的路径只有1个pair  输入eth，输出dai
+        const route = new Route([pair],weth);
+        console.log('route.midPrice',route.midPrice.toSignificant(6));
+
+        //定义交易，输入1个ether
+        const trade = new Trade(route, new TokenAmount(weth, "100000000000000000"), TradeType.EXACT_INPUT);
+        //计算产出/投入 即 DAI/ETHER
+        console.log('executionPrice',trade.executionPrice.toSignificant(6));
+        //计算假设没有滑点的中间价
+        console.log('nextMidPrice',trade.nextMidPrice.toSignificant(6));
+        //万分之五滑点
+        const slippageTolerance = new Percent('50', '10000');
+        //计算当前滑点下的换出的DAI最小值
+        const minimumAmountOut = trade.minimumAmountOut(slippageTolerance).raw;
+        console.log(`5/1000 slippageTolerance,minimumAmountOut: ${minimumAmountOut}`);
+
+
+
+
+        // const signer = new ethers.Wallet("PRIVATE_KEY");
+        const signer = daiWhileSigner;
+        const account = signer.connect(provider);
+
+        //构造交易所router2合约
+        const uniwap = new ethers.Contract(
+          mainnet_UniswapV2Router02_Address,
+          ['function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts);'],
+          account
+        );
+
+        // eth =》 dai
+        const path = [weth.address, dai.address];
+
+        const deadline = Math.floor(Date.now() / 1000) + 60 * 20;
+        const value = trade.inputAmount.raw;
+
+        console.log("swapExactETHForTokens: amountOutMin[%s],path[%s],to[%s],deadline[%s],value[%s]",amountOutMin,path,to,deadline,value);
+        const tx = await uniwap.swapExactETHForTokens(
+          amountOutMin,
+          path,
+          to,
+          deadline,
+          { value, gasPrice: 20e9 }
+        );
+        console.log(`Transaction hash: ${tx.hash}`);
+
+        const receipt = await tx.wait();
+        console.log(`Transaction was mined in block ${receipt.blockNumber}`);
+    });
+  });
+});
