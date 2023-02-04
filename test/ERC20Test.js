bdffd72a8f66bd9c8f0c9eb758505f0c0df57f72
forestfantacy <13717507049@163.com>
2023-02-04 13:48:28 +0800

    init

--- test/ERC20Test.js
+++ test/ERC20Test.js
@@ -6,11 +6,14 @@ const { ethers,network } = require("hardhat");
 
 describe("", function () {
 
-  const DAI_WHALE = '0xF977814e90dA44bFA03b6295A0616a897441aceC';
   const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+  const DAI_WHALE = '0xF977814e90dA44bFA03b6295A0616a897441aceC';
   const RECEIVER = '0x8C8D7C46219D9205f056f28fee5950aD564d7465';
-  let daiSigner
+
   let daiToken
+  let daiWhileAddr
+  let receiveAddr
+
 
   beforeEach(async () => {
 
@@ -21,9 +24,8 @@ describe("", function () {
     })
     daiToken = await ethers.getContractAt("IERC20",DAI);
 
-    //获取私钥
-    daiSigner = await ethers.getSigner(DAI);
-
+    daiWhileSigner= await ethers.getSigner(DAI_WHALE);
+    receiveSigner = await ethers.getSigner(RECEIVER);
   })
 
   describe("testxxx", function () {
@@ -32,7 +34,7 @@ describe("", function () {
         console.log("balanceReceiver:",await daiToken.balanceOf(RECEIVER));
     });
     it("testcase: DAI_WHALE send DAI to RECEIVE", async function () {
-        daiToken.transferFrom(DAI_WHALE,RECEIVE,100);
+        daiToken.transferFrom(address(daiWhileSigner.address, receiveAddr.address, 100);
         console.log("after transform balanceWhale:",await daiToken.balanceOf(DAI_WHALE));
         console.log("after transform balanceReceiver:",await daiToken.balanceOf(RECEIVER));
     });
