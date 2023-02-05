4776508b5c26b044c6dde99cf496c64f0ca7a359
forestfantacy <13717507049@163.com>
2023-02-05 16:46:34 +0800

    init

--- contracts/TestUniswap.sol
+++ contracts/TestUniswap.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.7.0;
+pragma solidity =0.6.6;
 
 import '@uniswap/v2-periphery/contracts/interfaces/IERC20.sol';
 import './interfaces/IUniswapV2Router.sol';
