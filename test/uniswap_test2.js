ad38f63d0bafc1874717086f40f585c45a0fa682
forestfantacy <13717507049@163.com>
2023-02-05 17:45:27 +0800

    init

--- test/uniswap_test2.js
+++ test/uniswap_test2.js
@@ -40,7 +40,7 @@ describe("", function () {
       console.log("11111");
 
 
-      await tokenIn.approve(testUniswap.address, AMOUNT_IN);
+      await tokenIn.approve(testUniswap.address, 10000);
       console.log("22222");
       const value2 = ethers.utils.parseUnits("2", "ether");
       const amountIn22 = ethers.utils.parseUnits("0.02", "ether");
