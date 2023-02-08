d1abd4c6eb4f496d3b8aef2f8b6defd4d096ae3c
forestfantacy <13717507049@163.com>
2023-02-08 21:13:06 +0800

    init

---
+++ test/FlashLoanSwap.js
@@ -0,0 +1,41 @@
+const { ethers,network } = require("hardhat");
+
+describe("Lock", function () {
+  describe("Swap", function () {
+    it("Test flash loan swap", async function () {
+      const [owner] = await ethers.getSigners();
+      const AmountToSwap = ethers.utils.parseEther('10');
+  
+      const FlashLoanSwap = await ethers.getContractFactory("FlashLoanSwap");
+      const flashLoanSwap = await FlashLoanSwap.deploy();
+
+      const weth = await ethers.getContractAt("IWETH", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2");
+      // 当前账户 给测试合约 发送10个以太
+      await owner.sendTransaction({
+        to: flashLoanSwap.address,
+        value: AmountToSwap
+      });
+
+      // weth 合约存入 10个以太
+      await weth.deposit({
+        value: AmountToSwap
+      });
+
+      // 当前账户 给测试合约 发送10个weth token
+      await weth.transfer(flashLoanSwap.address, AmountToSwap);
+      const wethERC20 = await ethers.getContractAt("IERC20", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2");
+      const balanceBeforeSwap = await wethERC20.balanceOf(flashLoanSwap.address);
+
+
+
+      // 测试逻辑
+      await flashLoanSwap.testFlashSwap(AmountToSwap);
+      const balanceAfterSwap = await wethERC20.balanceOf(flashLoanSwap.address);
+      console.log("Making WETH -> USDT -> LINK -> WETH flashloan swap");
+      console.log("Amount of WETH loan: ", AmountToSwap);
+      console.log("Amount of WETH before swap: ", balanceBeforeSwap);
+      console.log("Amount of WETH after swap: ", balanceAfterSwap);
+      console.log("Amount before swap - amount after swap: ", balanceBeforeSwap.sub(balanceAfterSwap));
+    });
+  });
+});
