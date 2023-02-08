75e836afaeeef1b1e096f7a27f7d93930cc1df5f
forestfantacy <13717507049@163.com>
2023-02-08 22:28:48 +0800

    init

--- test/FlashLoanSwap.js
+++ test/FlashLoanSwap.js
@@ -9,24 +9,31 @@ describe("Lock", function () {
       const FlashLoanSwap = await ethers.getContractFactory("FlashLoanSwap");
       const flashLoanSwap = await FlashLoanSwap.deploy();
 
+      //@uniswap/v2-periphery/contracts/interfaces/IWETH.sol
       const weth = await ethers.getContractAt("IWETH", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2");
-      // 当前账户 给测试合约 发送10个以太
-      await owner.sendTransaction({
-        to: flashLoanSwap.address,
-        value: AmountToSwap
-      });
+      
+      console.log("11   :",ethers.utils.formatEther(await owner.getBalance()));
 
-      // weth 合约存入 10个以太
+      // WETH 合约方法：给调用者 owner 添加 WETH
       await weth.deposit({
         value: AmountToSwap
       });
 
-      // 当前账户 给测试合约 发送10个weth token
+      console.log("22   :",ethers.utils.formatEther(await owner.getBalance()));
+      const flashLoanSwapSigner= await ethers.getSigner(flashLoanSwap.address);
+      console.log("33   :",ethers.utils.formatEther(await flashLoanSwapSigner.getBalance()));
+
+      // etherjs api： 当前账户 给测试合约 发送10个以太
+      await owner.sendTransaction({
+        to: flashLoanSwap.address,
+        value: AmountToSwap
+      });
+      console.log("44   :",ethers.utils.formatEther(await flashLoanSwapSigner.getBalance()));
+
+      // 当前账户owner 给测试合约 发送10个weth token
       await weth.transfer(flashLoanSwap.address, AmountToSwap);
       const wethERC20 = await ethers.getContractAt("IERC20", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2");
       const balanceBeforeSwap = await wethERC20.balanceOf(flashLoanSwap.address);
-
-
 
       // 测试逻辑
       await flashLoanSwap.testFlashSwap(AmountToSwap);
