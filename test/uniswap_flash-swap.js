1214d2877a93b2bdc12d571ed89efd09ed7390ae
forestfantacy <13717507049@163.com>
2023-02-09 14:36:37 +0800

    init

--- test/uniswap_flash-swap.js
+++ test/uniswap_flash-swap.js
@@ -3,8 +3,6 @@ const { ethers,network } = require("hardhat");
 
 describe("", function () {
 
-  const DAI_WHALE = '0xF977814e90dA44bFA03b6295A0616a897441aceC';
-
   const USDC_WHALE = '0xf9211FfBD6f741771393205c1c3F6D7d28B90F03';
 
   const BORROW_AMOUNT = 1000000; //6位小数，代表1个USDC
@@ -19,42 +17,18 @@ describe("", function () {
         method: "hardhat_impersonateAccount",
         params: [USDC_WHALE]
         })
-        await network.provider.request({
-          method: "hardhat_impersonateAccount",
-          params: [DAI_WHALE]
-          })
       const TestUniswapV2FlashSwap = await ethers.getContractFactory("TestUniswapV2FlashSwap");
       const testUniswapV2FlashSwap = await TestUniswapV2FlashSwap.deploy();
       await testUniswapV2FlashSwap.deployed();
 
-      whaleSigner = await ethers.getSigner(DAI_WHALE);
-      //冒充转账
-      await whaleSigner.sendTransaction({
-        to: testUniswapV2FlashSwap.address,
-        value: ethers.utils.parseUnits("1000", "ether"),
-      });
-      console.log('11111');
-      //先把WETH转给测试合约，为兑换USDC做准备，因为要用有WETH，才能借USDC
-      // const wethERC20 = await ethers.getContractAt("IERC20", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2");
-      // await wethERC20.transfer(testUniswapV2FlashSwap.address, ethers.utils.parseEther('10'));
-      //兑换和调用合约需要支付手续费
-      // const [owner, addr1 , addr2] = await ethers.getSigners();
-      // await owner.sendTransaction({
-      //   to: testUniswapV2FlashSwap.address,
-      //   value: ethers.utils.parseEther('3')
-      // });
-      console.log('22222');
-
       const usdcERC20 = await ethers.getContractAt("IERC20", BORROW_TOKEN_ADDR);
-      // const tokenUSDC = new ethers.Contract(USDC, abi, await ethers.getSigner(USDC));
 
       const usdcWhaleSigner = await ethers.getSigner(USDC_WHALE);
 
-      //先把USDC_WHAL USDC转给测试合约，
-      // console.log("usdc whale balance:%s", await usdcERC20.balanceOf(USDC_WHALE));
-      // await usdcERC20.connect(usdcWhaleSigner).transfer(testUniswapV2FlashSwap.address, BORROW_AMOUNT);
-      // await usdcERC20.transfer(testUniswapV2FlashSwap.address, ethers.utils.parseEther('10'));
-      console.log('3333');
+      //先把USDC_WHAL USDC转给测试合约，因为测试合约归还贷款的时候，需要 大约 3% * BORROW_AMOUNT 的USDC Token作为手续费
+      await usdcERC20.connect(usdcWhaleSigner).transfer(testUniswapV2FlashSwap.address, 10000000);
+      console.log("testUniswapV2FlashSwap.address usdc bal [%s]", await usdcERC20.balanceOf(testUniswapV2FlashSwap.address));
+
       // 执行flash swap 贷款
       const tx = await testUniswapV2FlashSwap.connect(usdcWhaleSigner).testFlashSwap(usdcERC20.address, BORROW_AMOUNT);
       const receipt = await tx.wait();
