871e32f358d6ad3e79e85442176b430263c1c16e
forestfantacy <13717507049@163.com>
2023-02-09 13:38:24 +0800

    init

--- contracts/TestUniswapV2FlashSwap.sol
+++ contracts/TestUniswapV2FlashSwap.sol
@@ -15,10 +15,10 @@ contract TestUniswapV2FlashSwap is IUniswapV2Callee{
 
     event Log(string message, uint val);
 
-    function testFlashSwap(address hypothecatedToken, uint amount) external {
+    function testFlashSwap(address borrowToken, uint amount) external {
 
         // 构造借出交易对 USDC-WETH
-        address pair = IUniswapV2Factory(FACTORY).getPair(hypothecatedToken, WETH);
+        address pair = IUniswapV2Factory(FACTORY).getPair(borrowToken, WETH);
         // 当前池中必须有交易对
         require(pair != address(0), "!pair");
 
@@ -27,14 +27,14 @@ contract TestUniswapV2FlashSwap is IUniswapV2Callee{
         address token1 = IUniswapV2Pair(pair).token1(); //WETH
 
         // 构建换出token数量   
-        uint amount0Out = hypothecatedToken == token0 ? amount : 0; //_amount
-        uint amount1Out = hypothecatedToken == token1 ? amount : 0; //0
+        uint amount0Out = borrowToken == token0 ? amount : 0; //_amount
+        uint amount1Out = borrowToken == token1 ? amount : 0; //0
 
         console.log("pair [%s], token0 [%s] token1 [%s]", pair, token0, token1);
         console.log("amount0Out [%s] amount1Out [%s]", amount0Out, amount1Out);
         
         //不为空则触发闪电贷流程
-        bytes memory data = abi.encode(hypothecatedToken, amount);
+        bytes memory data = abi.encode(borrowToken, amount);
 
         /**
          * 交易对借出指定数量USDC放到当前合约地址
@@ -43,14 +43,15 @@ contract TestUniswapV2FlashSwap is IUniswapV2Callee{
          * @param _amount1  0  有可能同时为空或者同时有值吗？
          * @param _data 
          */
-        IUniswapV2Pair(pair).swap(amount0Out, amount1Out, address(this), data);
+        // IUniswapV2Pair(pair).swap(amount0Out, amount1Out, address(this), data);
+        IUniswapV2Pair(pair).swap(1000000000, 0, address(this), data); // 借1000个USDC并发给当前合约
         // IUniswapV2Pair(pair).swap(1000000, 1000000, address(this), data); //amount0Out amount1Out 都有值是啥意思？
     }
 
     // 回调接口
     function uniswapV2Call(
         address _sender,
-        uint _amount0,
+        uint _amount0, 
         uint _amount1,
         bytes calldata _data
     )external override{
@@ -61,10 +62,9 @@ contract TestUniswapV2FlashSwap is IUniswapV2Callee{
         // 确认回调发起者是当前合约地址
         require(_sender == address(this), "!sender");
 
-        (address hypothecatedToken, uint amount) = abi.decode(_data, (address, uint));
+        (address borrowToken, uint amount) = abi.decode(_data, (address, uint));
 
-        console.log("cur contract [%s] has borrow [%s] WETH by [%s] USDC", address(this), IERC20(hypothecatedToken).balanceOf(address(this)), amount);
-        console.log("换出 [%s]token1 balance:",IERC20(token1).balanceOf(address(this)));
+        console.log("cur contract [%s] has borrow [%s] USDC", address(this), IERC20(borrowToken).balanceOf(address(this)));
 
         // 3%
         uint fee = (amount * 3 / 997) + 1;
@@ -72,14 +72,14 @@ contract TestUniswapV2FlashSwap is IUniswapV2Callee{
         uint amountToRepay = amount + fee;
 
         console.log("amount [%s] fee[%s] ", amount, fee);
-        emit Log("amount", amount);
+        emit Log("borrow amount", amount);
         emit Log("_amount0", _amount0);
         emit Log("_amount1", _amount1);
         emit Log("fee", fee);
         emit Log("amount to repay", amountToRepay);
 
         // 当前合约把 USDC 还给交易对
-        IERC20(hypothecatedToken).transfer(pair, amountToRepay);
+        IERC20(borrowToken).transfer(pair, amountToRepay);
 
         // 如果不还怎么检查并报错 UniswapV2: INSUFFICIENT_INPUT_AMOUNT
     }
--- test/uniswap_flash-swap.js
+++ test/uniswap_flash-swap.js
@@ -3,11 +3,11 @@ const { ethers,network } = require("hardhat");
 
 describe("", function () {
 
-  const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+
   const USDC_WHALE = '0xf9211FfBD6f741771393205c1c3F6D7d28B90F03';
 
-  const FUND_AMOUNT = 1000000; //6位小数
-  const HYPOTHECATED_USDC_AMOUNT = 2000000; 
+  const BORROW_AMOUNT = 1000000; //6位小数，代表1个USDC
+  const BORROW_TOKEN_ADDR = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'; //USDC
 
   describe("testxxx", function () {
 
@@ -23,21 +23,28 @@ describe("", function () {
       const testUniswapV2FlashSwap = await TestUniswapV2FlashSwap.deploy();
       await testUniswapV2FlashSwap.deployed();
 
-      const abi = [
-          "function balanceOf(address owner) view returns (uint256)",
-          "function transfer(address to, uint amount) returns (bool)"
-      ];
+      //先把WETH转给测试合约，为兑换USDC做准备，因为要用有WETH，才能借USDC
+      const wethERC20 = await ethers.getContractAt("IERC20", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2");
+      await wethERC20.transfer(testUniswapV2FlashSwap.address, ethers.utils.parseEther('10'));
+      //兑换和调用合约需要支付手续费
+      await owner.sendTransaction({
+        to: testUniswapV2FlashSwap.address,
+        value: ethers.utils.parseEther('3')
+      });
 
-      const tokenUSDC = new ethers.Contract(USDC, abi, await ethers.getSigner(USDC));
-      const usdcWhaleSigner= await ethers.getSigner(USDC_WHALE);
+
+      const usdcERC20 = await ethers.getContractAt("IERC20", BORROW_TOKEN_ADDR);
+      // const tokenUSDC = new ethers.Contract(USDC, abi, await ethers.getSigner(USDC));
+
+      const usdcWhaleSigner = await ethers.getSigner(USDC_WHALE);
 
       //先把USDC_WHAL USDC转给测试合约，
-      console.log("usdc whale balance:%s, fund:%s", await tokenUSDC.balanceOf(USDC_WHALE), FUND_AMOUNT);
-      await tokenUSDC.connect(usdcWhaleSigner).transfer(testUniswapV2FlashSwap.address, FUND_AMOUNT);
-      console.log("usdc whale transfer to testUniswapV2FlashSwap %s usdc", FUND_AMOUNT);
+      console.log("usdc whale balance:%s", await usdcERC20.balanceOf(USDC_WHALE));
+      // await usdcERC20.connect(usdcWhaleSigner).transfer(testUniswapV2FlashSwap.address, BORROW_AMOUNT);
+      await usdcERC20.transfer(testUniswapV2FlashSwap.address, ethers.utils.parseEther('10'));
 
-      // 执行flash swap 质押
-      const tx = await testUniswapV2FlashSwap.connect(usdcWhaleSigner).testFlashSwap(tokenUSDC.address, HYPOTHECATED_USDC_AMOUNT);
+      // 执行flash swap 贷款
+      const tx = await testUniswapV2FlashSwap.connect(usdcWhaleSigner).testFlashSwap(usdcERC20.address, BORROW_AMOUNT);
       const receipt = await tx.wait();
       console.log(`Transaction was mined in block ${receipt.blockNumber}`);
     });
